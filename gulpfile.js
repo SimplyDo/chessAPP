0191a2a1e794e4e17fe0a4943f4109247ea196b9
Alexander Kohlhofer <alexander@kohlhofer.com>
2014-11-16 22:06:46 -0500

    exchanged icons and consolidated navigation

--- gulpfile.js
+++ gulpfile.js
@@ -43,13 +43,13 @@ gulp.task('watch', function() {
 gulp.task('move',function(){
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
-  gulp.src(['app/bower_components/**/*','app/iconic/**/*','app/views/**/*','app/index.html','app/404.html'], { base: 'app/' })
+  gulp.src(['app/bower_components/**/*','app/css/fonts/**/*','app/views/**/*','app/index.html','app/404.html'], { base: 'app/' })
   .pipe(gulp.dest('dist/'));
 });
 
 
 gulp.task('styles', function() {
-  return gulp.src('app/styles/main.scss')
+  return gulp.src('app/css/main.scss')
     .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/css'))
